d85fc1eddfffc6a050d8f4bed29227cbb5731773
Keffmen <bylka500@gmail.com>
2023-07-19 16:43:36 +0700

    ToDo v0.0.4
    Передал код

--- Marcop.py
+++ Marcop.py
@@ -4,34 +4,44 @@ import Connect_DataBase
 
 
 def Marcop_main():
-    key_main=types.ReplyKeyboardMarkup(resize_keyboard=True).add(types.KeyboardButton('Список задач📋'),
-                                                                 types.KeyboardButton('Добавить задачу📝'),
-                                                                 types.KeyboardButton('Удалить задачу❌')).add(types.KeyboardButton('Время🕓'))
+    key_main = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
-    return key_main   #1)Добавить кнопку "Время"✔️
+    key_main.add(types.KeyboardButton('Список задач📋'),
+                 types.KeyboardButton('Добавить задачу📝'),
+                 types.KeyboardButton('Удалить задачу❌'))
+
+    key_main.insert(types.KeyboardButton('Время🕓'))
+
+    return key_main  # 1)Добавить кнопку "Время"✔️
+
 
 def AddTask():
-    Button_task=types.ReplyKeyboardMarkup(resize_keyboard=True).add(types.KeyboardButton('Добавить на сегодня'),
-                                                                    types.KeyboardButton('Добавить на другую дату'))
-    Button_task=Button_Back_Reply(Button_task)
+    Button_task = types.ReplyKeyboardMarkup(resize_keyboard=True).add(types.KeyboardButton('Добавить на сегодня'),
+                                                                      types.KeyboardButton('Добавить на другую дату'))
+    Button_task = Button_Back_Reply(Button_task)
     return Button_task
 
-def Button_Back_Reply(Back:types.ReplyKeyboardMarkup):
+
+def Button_Back_Reply(Back: types.ReplyKeyboardMarkup):
     Back.insert(types.KeyboardButton('Назад'))
     return Back
 
-# def Button_Back_Inline(Back:types.InlineKeyboardMarkup):
-#     Back.insert(types.KeyboardButton('Назад'))
-#     return Back
+
+def Button_Back_Inline(Back: types.InlineKeyboardMarkup):
+    Back.row(types.InlineKeyboardButton('Назад', callback_data='Назад'))
+    return Back
+
 
 def marcop_task_list_today(task_list):
     marcop = types.InlineKeyboardMarkup(row_width=1)
-    sorted_list = sorted(task_list,key=lambda x: x[2])
+    sorted_list = sorted(task_list, key=lambda x: x[2])
     for task in sorted_list:
         marcop.add(types.InlineKeyboardButton(f'{task[2]}', callback_data=f'{task[2]}:{task[0]}'))
     return marcop
 
+
 marcop_task_list_today(Connect_DataBase.all_Tasks())
+
 
 def confirmation():
     confirmation = types.InlineKeyboardMarkup()
@@ -39,14 +49,23 @@ def confirmation():
                      types.InlineKeyboardButton("Нет", callback_data="No"))
     return confirmation
 
-def timeInlineButton():
+
+def timeInlineButton(hour: int, min: int):
     timeInlineButton = types.InlineKeyboardMarkup()
-    timeInlineButton.add(types.InlineKeyboardButton("Кнопка 1", callback_data="bnt1"),
-                         types.InlineKeyboardButton("Кнопка 2", callback_data="bnt2"),
-                         types.InlineKeyboardButton("Кнопка 3", callback_data="bnt3"),
-                         types.InlineKeyboardButton("Кнопка 4", callback_data="bnt4"),
-                         types.InlineKeyboardButton("Кнопка 5", callback_data="bnt5")
-                         )
+
+
+    if hour == 0:
+        hour = 24
+    if min == -1:
+        min = 60
+    timeInlineButton.row(types.InlineKeyboardButton("➕ 1️⃣ Час", callback_data=f"time:{hour + 1}:{min}"),
+                         types.InlineKeyboardButton("➖ ️ 1️⃣ Час", callback_data=f"time:{hour - 1}:{min}"))
+
+    timeInlineButton.row(types.InlineKeyboardButton(f"{hour}:{min}", callback_data='0'))
+
+    timeInlineButton.row(types.InlineKeyboardButton("➕ 1️⃣ минута", callback_data=f"time:{hour}:{min + 1}"),
+                         types.InlineKeyboardButton("➖ ️ 1️⃣ минута", callback_data=f"time:{hour}:{min - 1}"))
+    timeInlineButton = Button_Back_Inline(timeInlineButton)
     return timeInlineButton
 
-#3)добавить инлайт кнопки, которые вызываются во 2-м пункте (5 шт)✔️
+# 3)добавить инлайт кнопки, которые вызываются во 2-м пункте (5 шт)✔️
--- maindot.py
+++ maindot.py
@@ -2,7 +2,6 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 
-
 import logging
 import Marcop
 import Connect_DataBase
@@ -13,7 +12,7 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[logging.FileHandler("Error.log"),
                               logging.StreamHandler()])
-bot = Bot(token=open("Canfic.txt",mode="r").read())
+bot = Bot(token=open("Canfic.txt", mode="r").read())
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
@@ -23,6 +22,7 @@ async def welcome(msg: types.Message):
     logging.info(f"Пользователь {msg.from_user.username} \n"
                  f"написал сообщение: {msg.text}")
 
+
 @dp.message_handler(content_types=["text"])
 async def main_menu(msg: types.Message, state: FSMContext):
     if msg.text == "Список задач📋":
@@ -30,7 +30,7 @@ async def main_menu(msg: types.Message, state: FSMContext):
         if All_Tasks:
             Today = datetime.now().date().strftime('%d/%m/%Y')
             result = [task for task in All_Tasks if Today == task[1]]
-            await msg.answer("Ваш список на сегодня",reply_markup=Marcop.marcop_task_list_today(result))
+            await msg.answer("Ваш список на сегодня", reply_markup=Marcop.marcop_task_list_today(result))
         else:
             await msg.answer("Задач нет")
     elif msg.text == "Добавить задачу📝":
@@ -40,14 +40,16 @@ async def main_menu(msg: types.Message, state: FSMContext):
         await msg.answer("Удалить задачу пока в разработке🤷🏻‍♀️........")
     elif msg.text == "Время🕓":
         await msg.answer("Время:",
-                         reply_markup=Marcop.timeInlineButton())
+                         reply_markup=Marcop.timeInlineButton(hour=00, min=00))
 
-#2)отработчик кнопки "Время", которая выводит сообщение и инлайт кнопками✔️
+    # 2)отработчик кнопки "Время", которая выводит сообщение и инлайт кнопками✔️
     else:
-        await msg.answer(f"Вы отправили: <span class='tg-spoiler'><ins><i>{msg.text}</i></ins></span>", parse_mode='HTML')
+        await msg.answer(f"Вы отправили: <span class='tg-spoiler'><ins><i>{msg.text}</i></ins></span>",
+                         parse_mode='HTML')
 
-@dp.message_handler(content_types=types.ContentType.ANY,state=Sates.Forma.title)
-async def task_title(msg:types.Message, state: FSMContext):
+
+@dp.message_handler(content_types=types.ContentType.ANY, state=Sates.Forma.title)
+async def task_title(msg: types.Message, state: FSMContext):
     if msg.content_type == types.ContentType.TEXT:
         await state.update_data(title=msg.text)
         await msg.answer("Записал")
@@ -57,8 +59,9 @@ async def task_title(msg:types.Message, state: FSMContext):
         await msg.answer(f"Вы отправили {msg.content_type}\n"
                          f"я могу обработать только текст")
 
-@dp.message_handler(content_types=types.ContentType.ANY,state=Sates.Forma.date)
-async def task_title(msg:types.Message, state: FSMContext):
+
+@dp.message_handler(content_types=types.ContentType.ANY, state=Sates.Forma.date)
+async def task_title(msg: types.Message, state: FSMContext):
     if msg.content_type == types.ContentType.TEXT:
         if msg.text.find("/"):
             await state.update_data(date=msg.text)
@@ -71,13 +74,14 @@ async def task_title(msg:types.Message, state: FSMContext):
         await msg.answer(f"Вы отправили {msg.content_type}\n"
                          f"я могу обработать только текст")
 
-@dp.message_handler(content_types=types.ContentType.ANY,state=Sates.Forma.time)
-async def task_title(msg:types.Message, state: FSMContext):
+
+@dp.message_handler(content_types=types.ContentType.ANY, state=Sates.Forma.time)
+async def task_title(msg: types.Message, state: FSMContext):
     if msg.content_type == types.ContentType.TEXT:
         if msg.text.find(":"):
             await state.update_data(time=msg.text)
             await msg.answer("Записал")
-            data=await state.get_data()
+            data = await state.get_data()
             await msg.answer("Проверьте правильность задачи:\n"
                              f"<b> название задачи </b>: {data['title']}\n"
                              f"<b> дата </b>: {data['date']}\n"
@@ -91,8 +95,9 @@ async def task_title(msg:types.Message, state: FSMContext):
         await msg.answer(f"Вы отправили {msg.content_type}\n"
                          f"я могу обработать только текст")
 
+
 @dp.callback_query_handler(state=Sates.Forma.time)
-async def callback_add_task(call:types.callback_query,state:FSMContext):
+async def callback_add_task(call: types.callback_query, state: FSMContext):
     if call.data == "Yes":
         await call.message.edit_text("Отлично! Записал!", reply_markup=None)
         await state.finish()
@@ -101,6 +106,16 @@ async def callback_add_task(call:types.callback_query,state:FSMContext):
         await state.finish()
 
 
+@dp.callback_query_handler(lambda c: c.data and c.data.startswith('time'))
+async def callback_time(call: types.callback_query):
+    msg, hour, min = call.data.split(':')
+    await call.message.edit_text('Время:', reply_markup=Marcop.timeInlineButton(int(hour), int(min)))
 
-if __name__=="__main__":
+
+@dp.callback_query_handler(text=['Назад'])
+async def callback_back(call: types.callback_query):
+    await call.message.delete()
+
+
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
